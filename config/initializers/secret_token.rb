95610f7a782244ecc670002a7621b4de48b62356
Adam Robbie <adam.robbie@gmail.com>
2012-06-22 14:37:13 -0400

    Styling added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GistMe::Application.config.secret_token = 'effff75ed9fafb2e6281a4bcbc75447791aadcb3e4d52219a32111071058411c00c98089e1d1105bb16533619fb0bf12a052a9ff204c64f69e961826c3c93fdf'
